dc8f7f1a9b242a5d48f5e3e8b2bb2250d4834938
Amartya <chakrab.amartya@gmail.com>
2018-04-08 22:14:59 -0400

    Infomap parsing and encoding

---
+++ InfomapEncoder.py
@@ -0,0 +1,48 @@
+import sys
+
+if __name__ == "__main__":
+    file_name = sys.argv[1]
+    modified_edge_list = sys.argv[2]
+    encoded_data = sys.argv[3]
+    add_first = int(sys.argv[4])
+    encoder = dict()
+    reverse_encoder = dict()
+    edge_dict = dict()
+    placed = set()
+    counter = 1
+    num_edges = 0
+    with open(file_name) as f:
+        for line in f:
+            result = line.rstrip().split(" ")
+            for a in range(0, len(result)):
+                if result[a] not in reverse_encoder:
+                    encoder[counter] = result[a]
+                    reverse_encoder[result[a]] = counter
+                    counter += 1
+                    edge_dict[reverse_encoder[result[a]]] = set()
+            for b in range(1, len(result)):
+                num_edges += 1
+                edge_dict[reverse_encoder[result[0]]].add(reverse_encoder[result[b]])
+
+    new_edges = open(modified_edge_list, 'wb')
+    a = edge_dict.keys()
+    a.sort()
+    if add_first == 1:
+        first_line = str(len(encoder))
+        first_line += (" " + str(num_edges) + "\n")
+        new_edges.write(first_line)
+
+    for c in a:
+        for d in edge_dict[c]:
+            current_line = str(c)
+            current_line += " " + str(d)
+            current_line += "\n"
+            new_edges.write(current_line)
+    new_edges.close()
+
+    coded_data = open(encoded_data, 'wb')
+    for c in range(1, len(encoder) + 1):
+        current_line = str(c)
+        current_line += (" " + encoder[c] + "\n")
+        coded_data.write(current_line)
+    coded_data.close()
---
+++ InfomapParser.py
@@ -0,0 +1,20 @@
+import sys
+
+if __name__ == "__main__":
+    tree_file = sys.argv[1]
+    tracker = dict()
+    with open(tree_file) as t:
+        for line in t:
+            if line[0] == '#':
+                continue
+            split_line = line.rstrip().split(" ")
+            final_assignment = split_line[0].split(":")[0]
+            node_name = int(split_line[3])
+            tracker[node_name] = final_assignment
+
+    output_file = sys.argv[2]
+    output_file = open(output_file, 'wb')
+    for num in range(1, len(tracker) + 1):
+        current_line = str(num) + " " + tracker[num] + "\n"
+        output_file.write(current_line)
+    output_file.close()
